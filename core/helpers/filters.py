d0334894a638ee94847766133641a3a684fdbfed
Phillip Malboeuf <phil@boeuf.coffee>
2017-03-24 10:46:20 -0400

    Hotels in index menu

--- core/helpers/filters.py
+++ core/helpers/filters.py
@@ -44,13 +44,13 @@ def editable(editable, collection, collection_name, parent_id=None):
 		if request.current_session_is_admin:	
 			return Markup(markup.format(parent_id, collection_name, str(collection['_id']), split[0], collection[split[0]]))
 		else:
-			return collection[split[0]]
+			return  Markup(collection[split[0]])
 
 	else:
 		if request.current_session_is_admin:
 			return Markup(markup.format(parent_id, collection_name, str(collection[split[0]]['_id']), split[1], collection[split[0]][split[1]]))
 		else:
-			return collection[split[0]][split[1]]
+			return Markup(collection[split[0]][split[1]])
 	
 
 
