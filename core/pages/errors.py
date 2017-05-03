a5e71ce78222a6d084706b03e8e8154e9b92e12a
Phillip Malboeuf <phil@boeuf.coffee>
2017-05-03 17:51:15 -0400

    Redirects and 404 FIX

--- core/pages/errors.py
+++ core/pages/errors.py
@@ -6,7 +6,9 @@ from core.models.cms.piece import Piece
 from core.models.cms.page import Page
 from core.models.hotels.hotel import Hotel
 
-from flask import request, abort
+from config.redirects import redirects
+
+from flask import request, abort, redirect
 from flask import render_template, json
 
 from werkzeug.contrib.cache import SimpleCache
@@ -14,30 +16,36 @@ from werkzeug.contrib.cache import SimpleCache
 import os
 
 
-
 app.caches['/errors'] = SimpleCache()
 
 @app.errorhandler(404)
 def not_found(error):
+
+
 	if 'application/json' in request.headers['Accept']:
 		return raise_error('server', 'not_found', 404, no_abort=True)
 
 	else:
-		cached_template = app.caches['/errors'].get(request.path)
-		if cached_template is None or app.config['DEBUG']:
-			response = {
-				'pieces': Piece._values(),
-				'pages': Page.list(),
-				'hotels': Hotel.list(),
-				'current_path': request.path,
-				'debugging': app.config['DEBUG']
-			}
-			response['pieces_json'] = json.dumps(response['pieces'], sort_keys=False, default=json_formater)
+		try:
+			return redirect(redirects[request.path])
 
-			render = render_template('errors/' + str(error.code) + '.html', **response)
-			app.caches['/errors'].set(request.path, render, timeout=0)
-			return render
+		except KeyError:
+			cached_template = app.caches['/errors'].get(request.path)
+			if cached_template is None or app.config['DEBUG']:
+				response = {
+					'pieces': Piece._values(),
+					'pages': Page.list(),
+					'hotels': Hotel.list(),
+					'current_path': request.path,
+					'debugging': app.config['DEBUG']
+				}
+				response['pieces_json'] = json.dumps(response['pieces'], sort_keys=False, default=json_formater)
+				response['lang_route'] = '/'
 
-		else:
-			return cached_template
+				render = render_template('errors/' + str(error.code) + '.html', **response)
+				app.caches['/errors'].set(request.path, render, timeout=0)
+				return render
+
+			else:
+				return cached_template
 
