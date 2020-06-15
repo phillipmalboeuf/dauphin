2f7d2401a48269bc970ba71861da23ce4f82e353
Phillip Malboeuf <phil@boeuf.coffee>
2020-06-15 19:40:34 -0400

    pages

--- core/models/cms/page.py
+++ core/models/cms/page.py
@@ -1,7 +1,7 @@
 
 from core import app
 from flask import request, abort
-from core.models.core.model import Model
+from core.models.core.content import Content
 from core.models.core.has_routes import HasRoutes
 from core.models.core.with_templates import WithTemplates
 
@@ -16,18 +16,18 @@ import markdown
 
 
 with app.app_context():
-	class Page(WithTemplates, HasRoutes, Model):
+	class Page(WithTemplates, HasRoutes, Content):
 
-		collection_name = 'pages'
-		collection_sort = [('order', 1)]
+		collection_name = 'page'
+		# collection_sort = [('order', 1)]
 
-		schema = {
-			'name': validation_rules['text'],
-			'route': validation_rules['text'],
-			'body': validation_rules['text'],
-			'hide_from_navigation': validation_rules['bool'],
-			'metadata': validation_rules['metadata']
-		}
+		# schema = {
+		# 	'name': validation_rules['text'],
+		# 	'route': validation_rules['text'],
+		# 	'body': validation_rules['text'],
+		# 	'hide_from_navigation': validation_rules['bool'],
+		# 	'metadata': validation_rules['metadata']
+		# }
 
 
 		endpoint = '/pages'
@@ -82,13 +82,13 @@ with app.app_context():
 
 		@classmethod
 		def preprocess(cls, document, lang=None):
-			for cache in app.caches:
-				app.caches[cache].clear()
+			# for cache in app.caches:
+			# 	app.caches[cache].clear()
 
-			try:
-				document['route'] = urllib.parse.quote_plus(document['route'].lower())
-			except KeyError:
-				pass
+			# try:
+			# 	document['route'] = urllib.parse.quote_plus(document['route'].lower())
+			# except KeyError:
+			# 	pass
 
 			return super().preprocess(document, lang)
 
